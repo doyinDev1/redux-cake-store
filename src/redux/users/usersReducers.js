fd4fada8f0fdc1ca6bce3ecd3483d1146aea1915
doyinDev1 <adedoyin.oyebanji@9ijakids.com>
2022-08-14 20:18:04 +0100

    new try

--- src/redux/users/usersReducers.js
+++ src/redux/users/usersReducers.js
@@ -9,17 +9,20 @@ const initialState ={
 const usersReducer = (state = initialState, action) => {
 
     switch(action.type){
-        case FETCH_USERS_REQUEST: return {
+        case FETCH_USERS_REQUEST: 
+        return {
             ...state,
             loading: true
         }
-        case FETCH_USERS_SUCCESS: return {
+        case FETCH_USERS_SUCCESS: 
+        return {
             loading: false,
             users: action.payload,
             error: ''
 
         }
-        case FETCH_USERS_FAILURE: return{
+        case FETCH_USERS_FAILURE: 
+        return{
             ...state,
                  users: [],
                  error: action.payload
